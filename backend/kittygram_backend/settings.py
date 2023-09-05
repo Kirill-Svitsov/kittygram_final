396f637ab1d1a147de68db865fbff338ffd9577e
Kirill Svitsov <user@MacBook-Air-2.Home>
2023-09-05 20:08:48 +0100

    Send to review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,21 +1,17 @@
 # flake8: noqa
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 SECRET_KEY = os.getenv('SECRET_KEY', 'ssidasddcdgdf')
 
-
-# DEBUG = False
 DEBUG = os.getenv('DEBUG') == 'True'
 
-
-# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kittygramsvitsov.hopto.org']
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split()
 
 INSTALLED_APPS = [
