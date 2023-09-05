a894685bab9f9865277344f0e7223c9f7a67cb99
Kirill Svitsov <user@MacBook-Air-2.Home>
2023-09-05 19:40:24 +0100

    Second try debug true

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = False
+DEBUG = True
+# test
 
 ALLOWED_HOSTS = []
 
